9386caaebaa84d86407fe83818aa95b3247b9be4
zhulj <928138768@qq.com>
2018-04-08 14:51:35 +0800

    update

--- 315 Count of Smaller Numbers After Self.js
+++ 315 Count of Smaller Numbers After Self.js
@@ -8,16 +8,18 @@
  * @return {number[]}
  */
 var countSmaller = function(nums) {
+    if(nums.length == 0) return [];
+    
     let count=[],compare=[],countNum=0;
-    for(let i=0;i<nums.length;i++){
-        compare=nums.slice(i+1)
-        for(let j=0;j<compare.length;j++){
-            if(compare[j]<nums[i]){
+    for(let i=0;i<nums.length-1;i++){
+        for(let j=i+1;j<nums.length;j++){
+             if(nums[j]<nums[i]){
                 countNum++
             }
         }
         count.push(countNum)
         countNum=0
     }
+    count.push(0)
     return count
 };
